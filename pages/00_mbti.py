05a7c5b9da800d1de667c5feca73d6c56f94db91
jeongaejee <141461560+jeongaejee@users.noreply.github.com>
2025-06-21 13:14:13 +0900

    Update and rename mbti.py to 00_mbti.py

---
+++ pages/00_mbti.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc21hZXd1dmVyazJ2dmljMnBndGhlNTJtMGcweG1jdXgzOGhraG05MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qG2aJyWDOLWtJ2r9cJ/giphy.gif")
